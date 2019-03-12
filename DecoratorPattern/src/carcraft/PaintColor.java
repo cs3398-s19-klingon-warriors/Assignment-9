affb462ddea7d6c4b5cdd62b58f7c200a5d8afa3
serena-marie <serena.gutierrez0@gmail.com>
2019-03-12 01:46:49 -0500

    Add PaintColor

---
+++ DecoratorPattern/src/carcraft/PaintColor.java
@@ -0,0 +1,19 @@
+package carcraft;
+/**
+ *
+ * @author Serena Gutierrez
+ */
+public class PaintColor extends CarOptions{
+   
+    public String color;
+
+    public PaintColor(ICar specialCar, String color) {
+        super(specialCar);
+        this.color = color;
+    }
+    
+    @Override
+    public String GetDescription(){
+        return "Painting car " +this.color+"!";
+    }
+}

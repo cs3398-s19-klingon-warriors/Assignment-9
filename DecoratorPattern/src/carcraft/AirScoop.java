d5945a0c79b6885e40d008b3451e153d7debe3d9
Sarah <sfg11@txstate.edu>
2019-03-12 00:58:42 -0500

    fixed spacing and shit

--- DecoratorPattern/src/carcraft/AirScoop.java
+++ DecoratorPattern/src/carcraft/AirScoop.java
@@ -1,22 +1,20 @@
 // AirScoop.java
 // @Author: Sarah Gibbons
-// Airscoop demonstrates the single responsibility principle; this guys options is printin a string 
+// Airscoop demonstrates the single responsibility principle; this guys options is printin a string
+
+
 package carcraft;
 
 public class AirScoop extends CarOptions() {
 
 	public String my_scoop;
 
-	public AirScoop(String scoop_foo)
-	{
+	public AirScoop(String scoop_foo) {
 		System.out.println("AirScoop Constructor... scoops the air?");
-
     this.scoop = scoop_foo;
 	}
 
 	@Override
-
-	public String getDescription()
-	{
-		return " with " + this.scoop + " scoopin' ";
+	public String getDescription() {
+		return "with" + this.scoop + "my_scoop";
 	}

6e3ddb04c315c5eda4b79246c2a0ce65854d37f2
Talon <tks49@txstate.edu>
2019-03-13 22:21:22 -0700

    Talon's Custom Main/ReadMe

--- DecoratorPattern/src/carcraft/CarMain.java
+++ DecoratorPattern/src/carcraft/CarMain.java
@@ -19,47 +19,43 @@ public class CarMain {
 	System.out.println(myBasic.getDescription());
 
 
-	System.out.println("\nNow let's make types out of this existing basic car.");
+	System.out.println("\nNow we'll make 6 super awesome custom cars.");
 
 	// custom car 1
-	System.out.println("\nWe'll do SportWheels(Basic)) with default sport constructor.");
-	// SportWheels with default constructor
-	ICar sporty = new SportWheels(myBasic) ;
-	System.out.println(sporty.getDescription()) ;
-
+	System.out.println("\n1. A SuperCharger(myBasic):");
+	// SuperCharger with default constructor
+	ICar superCharged = new SuperCharger(myBasic) ;
+	System.out.println(superCharged.getDescription()) ;
 
 	// custom car 2
-	System.out.println("\nNow SportWheels(Basic)) with parameterized sport constructor.");
-	// SportWheels with param constructor
-	ICar sportyExtra = new SportWheels(myBasic, "spinning rims and slick as heck") ;
-	System.out.println(sportyExtra.getDescription()) ;
+	System.out.println("\n2. A SuperCharger(LiftKit(myBasic)):");
+	// SuperCharger with a lift kit
+	ICar superLift = new SuperCharger(new LiftKit(myBasic)) ;
+	System.out.println(superLift.getDescription()) ;
  
 	// custom car 3
-	System.out.println("\nNow SuperCharger(AirScoop(Basic))");
-	// SuperCharger(AirScoop)
-	ICar chargeScoop = new SuperCharger(new AirScoop(myBasic)) ;
-	System.out.println(chargeScoop.getDescription()) ;
- 
-	
-	System.out.println("\nNow let's directly make custom cars without using the basic from earlier.");
+	System.out.println("\n3. A LiftKit(AirScoop(myBasic)):");
+	// Lift kit with an air scoop
+	ICar liftScoop = new LiftKit(new AirScoop(myBasic)) ;
+	System.out.println(liftScoop.getDescription()) ;
 	 
 	// custom car 4
-	System.out.println("\nWe'll do SportWheels(LiftKit(PaintColor)).");
+	System.out.println("\n4. A SuperCharger(AirScoop(PaintColor(myBasic))):");
 	// SportWheels(LiftKit(PaintColor(new Basic)))
-	ICar fancy = new SportWheels(new LiftKit(new PaintColor(new BasicCar(), "electric blue")));
-	System.out.println(fancy.getDescription()) ;
+	ICar superScoopPainted = new SuperCharger(new AirScoop(new PaintColor(myBasic, "terracotta")));
+	System.out.println(superScoopPainted.getDescription()) ;
 
 	// custom car 5
-	System.out.println("\nWe'll do LiftKit(SuperCharger).");
-	ICar highCharge = new LiftKit(new SuperCharger(new BasicCar())) ;
-	System.out.println(highCharge.getDescription()) ;
+	System.out.println("\n5. A SuperCharger(PaintColor(myBasic)):");
+	ICar paintedSuper = new SuperCharger(new PaintColor(myBasic, "perriwinkle"));
+	System.out.println(paintedSuper.getDescription());
 
 	// custom car 6
-	System.out.println("\nNow let's make a car with all 5 decorator classes!");
-	ICar ultimate = new AirScoop(new SuperCharger(new LiftKit(new PaintColor(new SportWheels(new BasicCar(), "snow tires"), "fire engine red")))) ;
-	System.out.println(ultimate.getDescription()) ;
+	System.out.println("\n6. An AirScoop(PaintColor(myBasic)):");
+	ICar paintedScoop = new AirScoop(new PaintColor(myBasic, "gatorade red"));
+	System.out.println(paintedScoop.getDescription()) ;
 
-	System.out.println("\nSo many good cars! Goodbye!");
+	System.out.println("\nThe options are endless! But not really.");
 
 	}
 
--- README.md
+++ README.md
@@ -6,4 +6,11 @@
 **Rachel**: SportWheels.java<br>
 **Abel**: LiftKit.java
 
-Successful build and run: 3/13, 11:04pm, with Rachel CarMain version.
+Successful build and run: 3/13, 10;09pm, with Talon CarMain version.
+
+**SOLID Principles Explained:**
+1. Our code follows the Single Responsibility Principle as each class is responsible for one custom aspect of a car being built. These classes can be added or removed and will have no effect on the performance of the other classes.
+2. Our code adheres to the Open-Close Principle as each new custom option can be added as a new class and the creator does not need to edit any additional files to implement it.
+3. Our code adheres to the Liskov's Substitution Principle as each new custom options adds to the BasicCar description, but does not change the functionality of BasicCar.
+4. Our code adheres to Interface Segregation as our ICar class is seperate and is used as the only interface necessary to perform custom car creating.
+5. Our code adheres to Dependency Inversion Principles as our custom option classes use the interface as an abstraction layer before being used by BasicCar.
